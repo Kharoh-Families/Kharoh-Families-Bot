0a7fae6576fa39694098c8bbd99607a17d36d278
Kharoh <Gaulu03@gmail.com>
2021-04-17 10:41:58 +0200

    command pingserver and server requests now working

--- src/lib/events/commands/pingServer.ts
+++ src/lib/events/commands/pingServer.ts
@@ -8,15 +8,14 @@ export default
     template: "!pingServer /",
 
     exec: (message: Message, ...args: any[]) => {
-      const request = new global.assets.ServerRequest({ name: 'ping', params: null, maxDelay: 10000 })
+      const request = new global.assets.ServerRequest({ name: 'pingServer', params: null, maxDelay: 10000 })
 
       const currentTimestamp = Date.now()
 
-      global.server.emit('ping')
-      // console.log(global.server)
-
       const response = request.sendRequest()
-      response.then(() => message.reply(`Voilà ! (${Date.now() - currentTimestamp}ms)`))
-      response.catch(() => message.reply('Quelque chose de mal est arrivé au serveur... (pas de réponse).'))
+
+      response
+        .then(() => message.reply(`Voilà ! (${Date.now() - currentTimestamp}ms)`))
+        .catch(() => message.reply('Quelque chose de mal est arrivé au serveur... (pas de réponse).'))
     }
   }
