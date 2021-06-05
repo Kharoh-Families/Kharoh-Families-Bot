875feecbc9e507803d0821825fe12a65e39c22e0
Kharoh <Gaulu03@gmail.com>
2021-06-05 16:47:10 +0200

    updated channel ids

--- src/assets/Config/Config.ts
+++ src/assets/Config/Config.ts
@@ -17,10 +17,10 @@ export default class Config {
    * The role IDs of the families roles in the discord server
    */
   public familiesID: { [key: string]: string } = {
-    pink: '829308762774962218',
-    blue: '829309368477417492',
-    yellow: '829309405396467722',
-    green: '829309423440625684',
+    pink: '848661385714204743',
+    blue: '848661405535305749',
+    yellow: '848661431976853505',
+    green: '848661454932279366',
   }
 
   public textChannelID: { [key: string]: string } = {
